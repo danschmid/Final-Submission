b9aca845ae81c8d98007048c6a9051bd88fd5d79
dm2112 <damo6246@colorado.edu>
2018-03-12 16:36:43 -0600

    unity

---
+++ New Unity Project/Assets/scripts/destruct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruct : MonoBehaviour {
+
+	public float time;
+
+	void Awake () {
+		if (gameObject != null) {
+			Destroy(gameObject, time);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/scripts/projectileController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileController : MonoBehaviour {
+
+	public float bubbleSpeed;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+		if (transform.rotation.z > 0) {
+			transform.Rotate(new Vector3(180f,0, 0));
+			rb.AddForce (new Vector2(-1,0) * bubbleSpeed, ForceMode2D.Impulse);
+			print (transform.localRotation.z);
+		} else {
+			rb.AddForce (new Vector2(1,0) * bubbleSpeed, ForceMode2D.Impulse);
+			print (transform.localRotation.z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
